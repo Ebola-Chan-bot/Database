f7f3ad2c1ee50d8a12df10097619b346a050efc1
埃博拉酱 <vhtmfu@outlook.com>
2024-08-17 17:34:01 +0800

    20240817

---
+++ +Database/Exception.m
@@ -0,0 +1,10 @@
+classdef Exception<MATLAB.Lang.IEnumerableException
+	enumeration
+		Table_or_column_not_found
+		UpdateTable_lacks_primary_column
+		UpdateTable_column_names_contain_backquotes
+		Table_not_found_or_primary_key_missing
+		Datetime_TimeZone_not_set
+		Unsupported_VariableType
+	end
+end
---
+++ +Database/Version.m
@@ -0,0 +1,4 @@
+function V = Version
+V.Me='v1.0.0';
+V.MatlabExtension='v19.0.0';
+V.MATLAB='R2024a';
